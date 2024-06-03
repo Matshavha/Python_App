9bffb3f49fa7d7b66d9debff9a04cb28c1d7107e
Matshavha <153765151+Matshavha@users.noreply.github.com>
2024-06-03 14:27:45 +0200

    App.py

--- App.py
+++ App.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html', name='World')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f'Hello, {name}!'
 
 if __name__ == '__main__':
     app.run(debug=True)
