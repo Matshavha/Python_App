f02cd03b8deed647f15ee871edb1f2727a76c313
Matshavha <153765151+Matshavha@users.noreply.github.com>
2024-06-03 18:58:31 +0200

    App.py

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 from flask_compress import Compress
 
 app = Flask(__name__)
@@ -6,17 +6,7 @@ Compress(app)
 
 @app.route('/')
 def map():
-    return render_template_string('''
-    <!doctype html>
-    <html>
-        <head>
-            <title>Map</title>
-        </head>
-        <body>
-            <iframe src="{{ url_for('static', filename='Voronoi_Map.html') }}" width="100%" height="600" loading="lazy"></iframe>
-        </body>
-    </html>
-    ''')
+    return render_template('Voronoi_Map.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
