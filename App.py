ae95a67e913df694e6a839da3f58235947212fe0
Matshavha <153765151+Matshavha@users.noreply.github.com>
2024-06-03 15:46:11 +0200

    App.py

--- App.py
+++ App.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html', name='World')
-
-@app.route('/form')
-def form():
-    return render_template('form.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    name = request.form['name']
-    return f'Hello, {name}!'
+def map():
+    # Replace the below URL with the URL of your map.html file in Azure Storage
+    map_url = "https://classificationmap.blob.core.windows.net/feederclassify/Classification_Map.html"
+    return render_template_string(f"""
+    <!doctype html>
+    <html>
+        <head>
+            <title>Map</title>
+        </head>
+        <body>
+            <iframe src="{map_url}" width="100%" height="600"></iframe>
+        </body>
+    </html>
+    """)
 
 if __name__ == '__main__':
     app.run(debug=True)
