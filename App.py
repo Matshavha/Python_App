0e2cdb9c2f1fad1794704d870580af77e0be1f9f
Matshavha <153765151+Matshavha@users.noreply.github.com>
2024-06-05 19:35:33 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,22 +1,10 @@
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/call_url', methods=['GET'])
-def call_url():
-    # The URL to call, you can replace this with your target URL
-    url = 'https://classify.azureedge.net/Feeders_Classification/Classification_Map.html'
-    
-    # Make a GET request to the specified URL
-    response = requests.get(url)
-    
-    # Return the response content and status code
-    return jsonify({
-        'status_code': response.status_code,
-        'content': response.text
-    })
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
-
