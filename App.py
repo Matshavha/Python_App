ae0f83a8feff7f74537e9f0b2ee8523b7b452cb9
Matshavha <153765151+Matshavha@users.noreply.github.com>
2024-06-06 13:44:10 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,35 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify
+import gzip
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html', title='Home')
+def index():
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html', title='About')
-
-@app.route('/classification-map', methods=['GET', 'POST'])
-def classification_map():
-    if request.method == 'POST':
-        area = request.form['area']
-        # Define locations for areas in Gauteng, South Africa
-        if area.lower() == 'johannesburg':
-            location = [-26.2041, 28.0473]
-        elif area.lower() == 'pretoria':
-            location = [-25.7479, 28.2293]
-        else:
-            # Default location if the input doesn't match known areas
-            location = [-26.2041, 28.0473]  # Johannesburg as default
-
-        return render_template('map.html', location=location)
-    
-    return render_template('classification_map.html', title='Classification Map')
-
-@app.route('/prediction')
-def prediction():
-    return render_template('prediction.html', title='Prediction')
+@app.route('/geojson')
+def geojson():
+    with gzip.open('Classification_Map.geojson.gz', 'rt', encoding='utf-8') as f:
+        geojson_data = json.load(f)
+    return jsonify(geojson_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
