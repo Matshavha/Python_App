64d231ae852da42f6486cc406fb7398e7c4018a0
Matshavha <153765151+Matshavha@users.noreply.github.com>
2024-06-07 14:43:58 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def display_map():
-    return render_template('map.html')
+    response = make_response(render_template('map.html'))
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
-
-
