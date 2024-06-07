2baee8c77e541200a0b0b54a2fe843c22f1cf49e
Matshavha <153765151+Matshavha@users.noreply.github.com>
2024-06-07 11:07:55 +0200

    Update App.py

--- App.py
+++ App.py
@@ -3,13 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/map')
 def display_map():
     return render_template('map.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
+
 
