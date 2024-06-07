ce47d26e9daf0e16dfe772c8fa8ec5ebada1a2aa
Matshavha <153765151+Matshavha@users.noreply.github.com>
2024-06-07 15:31:29 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def display_map():
-    response = make_response(render_template('map.html'))
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '0'
-    return response
+def home():
+    return render_template('home.html')
+
+@app.route('/classification-map')
+def classification_map():
+    return render_template('classification_map.html')
+
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
+
+@app.route('/prediction')
+def prediction():
+    return render_template('prediction.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
